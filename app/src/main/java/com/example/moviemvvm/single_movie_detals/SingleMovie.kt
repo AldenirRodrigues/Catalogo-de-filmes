14296fffe4dc5623a52492675b2822aea4664b08
Aldenir <aldenir_19@hotmail.com>
2020-06-27 11:46:30 -0300

    Implementação das bibliotecas e criação da tela de descrição

---
+++ app/src/main/java/com/example/moviemvvm/single_movie_detals/SingleMovie.kt
@@ -0,0 +1,18 @@
+package com.example.moviemvvm.single_movie_detals
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import butterknife.BindView
+import com.example.moviemvvm.R
+
+class SingleMovie : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+
+    }
+
+}
